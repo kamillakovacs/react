34540e6233eafd0a00c09cb0b6dea2aae0fc6e0b
Kamilla Kovacs <knkovacs@gmail.com>
2018-12-11 16:24:14 +0100

    UI component - conditionally inputting into template

--- react-app1/app1/src/Amaze.js
+++ react-app1/app1/src/Amaze.js
@@ -3,18 +3,33 @@ import React, { Component } from 'react';
 
 // this is a functional/UI component
 const Amaze = ({amaze}) => {
-  const amazingList = amaze.map(amaze => {
-    return (
+  // const amazeballs = amaze.map(amaze => {
+  //   if (amaze.age > 20) {
+  //     return (
+  //       <div className="amaze" key={ amaze.id }> 
+  //         <div>Name: { amaze.name }</div>
+  //         <div>Age: { amaze.age }</div>
+  //         <div>Gender: { amaze.gender }</div>
+  //       </div>
+  //     )
+  //   } else {
+  //     return null;
+  //   }
+  // })
+
+  const amazeballs = amaze.map(amaze => {
+    return amaze.age > 20 ? (
       <div className="amaze" key={ amaze.id }> 
         <div>Name: { amaze.name }</div>
         <div>Age: { amaze.age }</div>
         <div>Gender: { amaze.gender }</div>
       </div>
-    )
+    ) : null;
   })
+  
   return (
     <div className='amazing-list'>
-      { amazingList }
+      { amazeballs }
     </div>
   );
 };
