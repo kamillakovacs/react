73c161be4f303fc5a1250f7e804b1543e5006fd8
Kamilla Kovacs <knkovacs@gmail.com>
2018-12-11 11:29:18 +0100

    fix

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'),
-  app = express(), 
-  PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`App is up and running on port ${PORT}`);
-});
-
-module.exports = app;
