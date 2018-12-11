7d0d60495168590034bce453c79700cd0dc24bb5
Kamilla Kovacs <knkovacs@gmail.com>
2018-12-11 16:12:11 +0100

    UI component with props as parameter

--- react-app1/app1/src/Amaze.js
+++ react-app1/app1/src/Amaze.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 
-class Amaze extends Component {
 
-
-  render() {
-    const { amaze } = this.props;
-    const amazingList = amaze.map(amaze => {
-      return (
-        <div className="amaze" key={ amaze.id }> 
-          <div>Name: { amaze.name }</div>
-          <div>Age: { amaze.age }</div>
-          <div>Gender: { amaze.gender }</div>
-        </div>
-      )
-    })
+// this is a functional/UI component
+const Amaze = (props) => {
+  const { amaze } = props;
+  const amazingList = amaze.map(amaze => {
     return (
-      <div className='amazing-list'>
-        { amazingList }
+      <div className="amaze" key={ amaze.id }> 
+        <div>Name: { amaze.name }</div>
+        <div>Age: { amaze.age }</div>
+        <div>Gender: { amaze.gender }</div>
       </div>
-    );
-  }
-}
+    )
+  })
+  return (
+    <div className='amazing-list'>
+      { amazingList }
+    </div>
+  );
+};
 
 export default Amaze;
